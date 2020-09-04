18f0f2f2a801fc1ce2c35c10311dc3629e632ead
Anton <anton.chivkunov@mera.com>
2020-09-04 14:40:16 +0300

    add images.go

---
+++ images.go
@@ -0,0 +1,49 @@
+package main
+
+import (
+	"image"
+	"image/color"
+
+	"golang.org/x/tour/pic"
+)
+
+func Pic(dx, dy int) [][]uint8 {
+	result := make([][]uint8, dy)
+	for i := 0; i < dy; i++ {
+		result[i] = make([]uint8, dx)
+	}
+
+	for x := 0; x < dx; x++ {
+		for y := 0; y < dy; y++ {
+			result[x][y] = uint8(x ^ y)
+		}
+	}
+	return result
+}
+
+type Image struct {
+	w, h int
+}
+
+func (im Image) Bounds() image.Rectangle {
+	return image.Rect(0, 0, im.w, im.h)
+}
+
+func (im Image) ColorModel() color.Model {
+	return color.RGBAModel
+}
+
+func (im Image) At(x, y int) color.Color {
+	img_func := func(x, y int) uint8 {
+		//return uint8(x*y)
+		//return uint8((x+y) / 2)
+		return uint8(x ^ y)
+	}
+	v := img_func(x, y)
+	return color.RGBA{v, v, v, 255}
+}
+
+func main() {
+	m := Image{128, 128}
+	pic.ShowImage(m)
+}
